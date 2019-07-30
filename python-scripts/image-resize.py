f493219c268fb73d0f9b2f5efc1e6c9144949f58
Alex Gourlay <alexdgourlay@gmail.com>
2019-07-30 19:22:12 +0200

    Began implemenrtation of iterating through resource directory

--- python-scripts/image-resize.py
+++ python-scripts/image-resize.py
@@ -1,12 +1,44 @@
 from PIL import Image
+import os
+import imghdr
 
-RES_DIR = "../alexgourlay_com/resources"
+RES_DIR = "../alexgourlay_com/resources/"
 
-im = Image.open('Handy.PNG')
+for root, dirs, files in os.walk(RES_DIR):
+    for file in files:
+        if imghdr.what(os.path.join(root, file)):
+            print("Image Found: {}".format(file))
 
-box = (100, 100, 400, 400)
-region = im.crop(box)
+# Asigned height of optimised images.
+height = 1000
 
-out = im.point(lambda i: i * 20)
+# Function for printing number in readable byte format e.g. '1.2KiB'
+def sizeof_fmt(num, suffix='b'):
+    for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
+        if abs(num) < 1024.0:
+            return "%3.1f%s%s" % (num, unit, suffix)
+        num /= 1024.0
+    return "%.1f%s%s" % (num, 'Yi', suffix)
 
-out.show()
+def run():
+    IN_PATH = './murzuq.jpg'
+
+    im = Image.open(IN_PATH)
+
+    # print("Original: " + "{}, ".format(im.size) + sizeof_fmt(os.path.getsize(IN_PATH)))
+
+    # Convert to RGB format if not already in that format.
+    if not IN_PATH.endswith(('.jpg', '.jpeg', '.JPG', '.JPEG')):
+        im = im.convert("RGB")
+
+    im_size = im.size
+    im_ratio = im_size[1]/im_size[0]
+
+    out = im.resize((height, round(im_ratio*height)), Image.ANTIALIAS)
+
+    OUT_PATH = "murzuq_opt.jpg"
+    out.save(OUT_PATH, quality= 90)
+
+    im = Image.open(OUT_PATH)
+    # print("Optimised: " + "{}, ".format(im.size) + sizeof_fmt(os.path.getsize(OUT_PATH)))
+
