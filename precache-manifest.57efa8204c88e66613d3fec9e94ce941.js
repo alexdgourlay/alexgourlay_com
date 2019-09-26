2ec4a3337aa94c9cd41e10c1f612509a5efb6388
Alex Gourlay <alexdgourlay@gmail.com>
2019-09-26 20:07:57 +0100

    Updates

---
+++ precache-manifest.57efa8204c88e66613d3fec9e94ce941.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e77c2bf6c30bb486b775d361036d08e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9d513f8d7d66824171e",
+    "url": "/static/css/main.247f5ca3.chunk.css"
+  },
+  {
+    "revision": "84fd2e76b550d51d0492",
+    "url": "/static/js/2.28c354e2.chunk.js"
+  },
+  {
+    "revision": "f23671f53cba5875e9a0",
+    "url": "/static/js/3.07ecb916.chunk.js"
+  },
+  {
+    "revision": "8940a374fe3872658bb5",
+    "url": "/static/js/4.e9b4a067.chunk.js"
+  },
+  {
+    "revision": "7fb55bda2cd53d441d06",
+    "url": "/static/js/5.99a72a5a.chunk.js"
+  },
+  {
+    "revision": "ed43f9b9a13d9a2d1101",
+    "url": "/static/js/6.154c3c6b.chunk.js"
+  },
+  {
+    "revision": "b9d513f8d7d66824171e",
+    "url": "/static/js/main.89360ca0.chunk.js"
+  },
+  {
+    "revision": "a8b5b5808d76fbe8651f",
+    "url": "/static/js/runtime~main.4c134d11.js"
+  }
+]);
