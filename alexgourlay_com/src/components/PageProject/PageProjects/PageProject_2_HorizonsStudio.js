a435299766648d0f33978d4c44ac75329afd849d
Alex Gourlay <alexdgourlay@gmail.com>
2019-10-03 22:21:43 +0100

    Updates

--- alexgourlay_com/src/components/PageProject/PageProjects/PageProject_2_HorizonsStudio.js
+++ alexgourlay_com/src/components/PageProject/PageProjects/PageProject_2_HorizonsStudio.js
@@ -1,11 +1,53 @@
 import React from 'react';
+import LazyLoad from 'react-lazyload';
+import Gallery from 'react-grid-gallery';
 
 import '../PageProject.css'
 import MyIFrame from '../../MyIFrame/MyIFrame';
 
 const PageProject_2_HorizonsStudio = (props) => {
 
+    const IMAGES_A = [
+        {
+            src: `${props.RES_DIR}/OPT_HD_ShoalFishStill.jpg`,
+            thumbnail: `${props.RES_DIR}/OPT_HD_ShoalFishStill.jpg`,
+            thumbnailWidth: 320,
+            thumbnailHeight: 320,
+        },
+        {
+            src: `${props.RES_DIR}/1.png`,
+            thumbnail: `${props.RES_DIR}/1.png`,
+            thumbnailWidth: 255,
+            thumbnailHeight: 320,
+        },
+        {
+            src: `${props.RES_DIR}/2.png`,
+            thumbnail: `${props.RES_DIR}/2.png`,
+            thumbnailWidth: 320,
+            thumbnailHeight: 320,
+        },
+        {
+            src: `${props.RES_DIR}/3.png`,
+            thumbnail: `${props.RES_DIR}/3.png`,
+            thumbnailWidth: 255,
+            thumbnailHeight: 320,
+        },
+        {
+            src: `${props.RES_DIR}/4.jpg`,
+            thumbnail: `${props.RES_DIR}/4.jpg`,
+            thumbnailWidth: 719,
+            thumbnailHeight: 320,
+        },
+        {
+            src: `${props.RES_DIR}/0.png`,
+            thumbnail: `${props.RES_DIR}/0.png`,
+            thumbnailWidth: 449,
+            thumbnailHeight: 320,
+        },
+    ];
+
     return (
+
         <div id='project-content'>
             <p className='p'>
                 During the summer of 2018 I interned at London based virtual reality
@@ -28,12 +70,19 @@ const PageProject_2_HorizonsStudio = (props) => {
                 _src="https://player.vimeo.com/video/361057084" />
 
             <p className='p'>
-
-            <p className='p'>
-                
-            </p>
+                Interactions and assets were developed inside Unity.
 
             </p>
+
+            <LazyLoad height={200}>
+                <div style={{"border":"dashed 2px blue", "width":"100%"}}>
+                    <Gallery
+                        images={IMAGES_A}
+                        enableImageSelection={false}
+                        margin={4}
+                        rowHeight={200} />
+                </div>
+            </LazyLoad>
 
 
         </div >
